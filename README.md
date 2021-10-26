df50fd3ff8a054e012203196bf12ff5f77cd3c04
Dan Walker <dan@pdxrevolution.com>
2021-10-26 05:29:35 -0700

    Assorted fixes After doing some testing, I found that the start date was being provided wrong. Updated it to be correct. Also updated the manual weight entry. Added the ability to get get all weight since a specific date, and the latest weight.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # weightgurus
 API Interface for Weight Gurus
+
+This is a simple API interface for Weight Gurus, 
+it allows you to pull down your entire weight history 
+and submit new weights.
+
+Weight Gurus doesn't have any documentation for their API,
+so this does the best we can with the little information we have.
+
+## Usage
+
+When using stand-alone you need to add your username and password
+to the main method, then either use 'get_all'
--- main.py
+++ main.py
@@ -5,6 +5,25 @@ import requests
 import json
 
 
+def transform_weight(weight):
+    """
+    Transforms normal weight in pounds to Weight Gurus format
+
+    :param weight: Weight in pounds
+    :return: Weight Guru format
+    """
+    try:
+        weight = str(weight).replace('.', '')
+        transformed_weight = int(weight)
+        return transformed_weight
+    except ValueError:
+        try:
+            transformed_weight = int(float(weight))
+            return transformed_weight
+        except ValueError:
+            print(f"Caught ValueError! '{weight}' is not a number.")
+
+
 class WeightGurus:
 
     def __init__(self, username, password):
@@ -14,7 +33,7 @@ class WeightGurus:
             'web': True
         }
         self.headers = None
-        self.start_date = {'start': '1970-01-01T01:00:00.504Z'}
+        self.start_date = 'start=1970-01-01T01:00:00.504Z'
         self.weight_history = None
         self.add_weight = {}
 
@@ -27,8 +46,10 @@ class WeightGurus:
         except Exception as e:
             print(f"Caught Exception reading JSON: {e}")
 
-    def __get_weight_history(self):
-        req = requests.get('https://api.weightgurus.com/v3/operation/', data=self.start_date, headers=self.headers)
+    def __get_weight_history(self, start_date=None):
+        if start_date:
+            self.start_date = f"start={start_date}"
+        req = requests.get(f'https://api.weightgurus.com/v3/operation/?{self.start_date}', headers=self.headers)
         try:
             json_data = req.json()
         except Exception as e:
@@ -38,18 +59,21 @@ class WeightGurus:
 
     def get_all(self):
         self.__do_login()
-        print(json.dumps(self.__get_weight_history(), indent=4, sort_keys=True))
+        return json.dumps(self.__get_weight_history(), indent=4, sort_keys=True)
 
-    @staticmethod
-    def format_time():
-        t = datetime.now()
-        s = t.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return s[:-3] + "Z"
+    def get_since_date(self, start_date):
+        self.__do_login()
+        return json.dumps(self.__get_weight_history(start_date), indent=4, sort_keys=True)
+
+    def get_latest(self):
+        self.__do_login()
+        weight_data = self.__get_weight_history()
+        return json.dumps(weight_data['operations'][-1])
 
     def manual_entry(self, weight, bmi=None, body_fat=None, muscle_mass=None, water=None):
         """
         Weight Gurus has a weird way of adding weight, they want integer values (numbers) without decimals.
-        This requires you to take you weight and remove the decimal. So, 100.0 pounds would be 1000 when you add it
+        This requires you to take you weight and remove the decimal. So, 100.0 pounds would be 1000 when you add it.
         """
         self.__do_login()
         self.headers['user-agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, ' \
@@ -59,7 +83,7 @@ class WeightGurus:
             self.add_weight['bmi'] = int(bmi)
         if body_fat:
             self.add_weight['bodyFat'] = int(body_fat)
-        self.add_weight['entryTimestamp'] = self.format_time()
+        self.add_weight['entryTimestamp'] = str(datetime.now())
         if muscle_mass:
             self.add_weight['muscleMass'] = int(muscle_mass)
         self.add_weight['operationType'] = 'create'
@@ -67,18 +91,18 @@ class WeightGurus:
         if water:
             self.add_weight['water'] = int(water)
         if weight is None:
-            print("You need to provide weight")
+            print("You need to provide your weight")
             exit(1)
-        elif len(str(weight)) < 4:
-            print("Weight gurus uses four digit integers for weight. So, 100 pounds would be 1000 (Think, 100.0)")
-            exit(1)
-        self.add_weight['weight'] = int(weight)
+            return False
+        weight = transform_weight(weight)
+        self.add_weight['weight'] = weight
         req = requests.post('https://api.weightgurus.com/v3/operation',
                             data=json.dumps(self.add_weight),
                             headers=self.headers)
         status_code = req.status_code
         if status_code == 201:
             print("Successfully added weight!")
+            return req.json()
 
 
 if __name__ == "__main__":
